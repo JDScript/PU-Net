a16aa2777e1d894fc7e85a52b08d2d38f914c3e2
JDScript <jd@jdscript.net>
2023-01-12 12:12:09 +0800

    remove -fPIC for nvcc

--- code/tf_ops/CD/tf_cd_compile_abi.sh
+++ code/tf_ops/CD/tf_cd_compile_abi.sh
@@ -5,7 +5,7 @@ TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())'
 TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 $nvcc tf_nndistance_g.cu -c -o tf_nndistance_g.cu.o  -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++17  -I $TF_INC -DGOOGLE_CUDA=1\
- -x cu -Xcompiler -ltensorflow_framework -fPIC -O2
+ -x cu -Xcompiler -ltensorflow_framework -O2
 
 g++ tf_nndistance.cpp tf_nndistance_g.cu.o -o tf_nndistance_so.so -std=c++17 -shared -ltensorflow_framework -fPIC -I $TF_INC \
 -I$TF_INC/external/nsync/public -L$TF_LIB -ltensorflow_framework -fPIC -lcudart -L $cudalib -O2 -D_GLIBCXX_USE_CXX11_ABI=0
