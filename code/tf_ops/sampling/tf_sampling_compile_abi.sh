ffedac821c313dcbdd50b8b4b3031a4becbca4a9
JDScript <jd@jdscript.net>
2023-01-12 12:08:21 +0800

    -fPIC

--- code/tf_ops/sampling/tf_sampling_compile_abi.sh
+++ code/tf_ops/sampling/tf_sampling_compile_abi.sh
@@ -6,7 +6,7 @@ TF_INC=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())'
 TF_LIB=$(python3 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 $nvcc tf_sampling_g.cu -c -o tf_sampling_g.cu.o  -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++17  -I $TF_INC -DGOOGLE_CUDA=1\
- -x cu -Xcompiler -ltensorflow_framework -O2
+ -x cu -Xcompiler -ltensorflow_framework -fPIC -O2
 
-g++ tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -std=c++17 -shared -ltensorflow_framework -I $TF_INC \
--I$TF_INC/external/nsync/public -I $cudainc -L$TF_LIB -ltensorflow_framework -lcudart -L $cudalib -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -std=c++17 -shared -ltensorflow_framework -fPIC -I $TF_INC \
+-I$TF_INC/external/nsync/public -I $cudainc -L$TF_LIB -ltensorflow_framework -fPIC -lcudart -L $cudalib -O2 -D_GLIBCXX_USE_CXX11_ABI=0
